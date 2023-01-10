974e90954c1ec0bf5a9931be8d92478b4f882894
fouad ajayi <fouadajayi6@gmail.com>
2023-01-10 22:21:29 +0100

    Ab

--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -3,12 +3,14 @@ import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+
   let saveExpenseDataHandler = (enteredExpenseData) => {
     let expenseData = {
       ...enteredExpenseData,
       id : Math.random().toString()
     }
+    props.onAddExpense(expenseData)
   };
 
   return (
@@ -16,6 +18,7 @@ const NewExpense = () => {
       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
     </div>
   );
+
 };
 
 export default NewExpense;
