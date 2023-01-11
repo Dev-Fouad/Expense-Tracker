a4097f63b92ffb96b8c9e2ecf8283e1c126c01f1
fouad ajayi <fouadajayi6@gmail.com>
2023-01-11 18:26:41 +0100

    Latest commit

--- src/Components/Expenses/ExpenseFilter.js
+++ src/Components/Expenses/ExpenseFilter.js
@@ -16,7 +16,7 @@ let ExpensesFilter = (props) => {
                   <option value='2021'>2021</option>
                   <option value='2020'>2020</option>
                   <option value='2019'>2019</option>
-                </select>
+               </select>
           </div>
        </div>
     )
