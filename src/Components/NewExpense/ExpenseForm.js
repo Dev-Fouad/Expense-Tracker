7776b7ac9e1e31c3987baa123a81c1784cdc2b8e
fouad ajayi <fouadajayi6@gmail.com>
2023-01-11 14:57:07 +0100

    New commit

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -24,7 +24,7 @@ const ExpenseForm = (props) => {
 
     let expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate)
     }
 
